8f2380b91e7fdb9be66ec6151983a21853241185
Vas <v.sliouniaev@gmail.com>
2016-02-09 21:45:12 +0000

    Add back-end functionality from pass-cache

---
+++ src/pass-cache-2/Middleware/FilterLinkProbes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace pass_cache_2.Middleware
+{
+    public class FilterLinkProbes
+    {
+        private static readonly string[] BlockedAgents = { "skype", "whatsapp" };
+        private readonly RequestDelegate _next;
+
+        public FilterLinkProbes(RequestDelegate next)
+        {
+            if (next == null)
+                throw new ArgumentNullException(nameof(next));
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            StringValues userAgents;
+            if (context.Request.Headers.TryGetValue("User-Agent", out userAgents))
+            {
+                if (userAgents.Select(a => a.ToLowerInvariant()).Any(userAgent => BlockedAgents.Any(blockedAgent => blockedAgent.Contains(userAgent))))
+                {
+                    context.Response.Redirect("/", true);
+                }
+            }
+
+            return _next(context);
+        }
+    }
+}
